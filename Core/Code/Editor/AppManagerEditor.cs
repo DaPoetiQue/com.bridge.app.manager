03eed59737327969c813baaa968f31c5859ce1ab
Da PoetiQue Rea7on Royal Nele <hlulani21@gmail.com>
2021-07-06 01:14:18 +0200

    Fix: Updated the manager editor script

--- Core/Code/Editor/AppManagerEditor.cs
+++ Core/Code/Editor/AppManagerEditor.cs
@@ -4,10 +4,10 @@ namespace Bridge.Core.App.Manager
 {
     public class AppManagerEditor : Editor
     {
-        [MenuItem("Bridge/Create/App Manager")]
+        [MenuItem("3ridge/Create/App Manager")]
         private static void CreateAppManager()
         {
-            var appManager = new UnityEngine.GameObject("_App Manager");
+            var appManager = new UnityEngine.GameObject("_3ridge App Manager");
             appManager.AddComponent<AppManager>();
 
             if(Selection.activeGameObject != null) appManager.transform.SetParent(Selection.activeGameObject.transform);
@@ -15,7 +15,7 @@ namespace Bridge.Core.App.Manager
             UnityEngine.Debug.Log("<color=white>-->></color> <color=green> Success </color>:<color=white> An app manager has been created successfully.</color>");
         }
 
-        [MenuItem("Bridge/Create/App Manager", true)]
+        [MenuItem("3ridge/Create/App Manager", true)]
         private static bool CanCreateAppManager()
         {
             return FindObjectOfType<AppManager>() == null;
